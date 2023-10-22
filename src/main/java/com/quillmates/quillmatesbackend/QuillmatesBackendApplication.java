26cd292dc4c593c751bb6b2859b847228d3acf13
AnaMaghear <ana.maghear@yahoo.com>
2023-10-22 19:10:46 +0300

    init project

---
+++ src/main/java/com/quillmates/quillmatesbackend/QuillmatesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quillmates.quillmatesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuillmatesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuillmatesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quillmates/quillmatesbackend/QuillmatesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quillmates.quillmatesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuillmatesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
